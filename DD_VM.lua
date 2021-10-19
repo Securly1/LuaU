a4d62d50e904e399c4d6949085db5f7e0903d605
Securly <91905863+Securly1@users.noreply.github.com>
2021-10-19 13:08:11 -0400

    Update DD_VM.lua

--- DD_VM.lua
+++ DD_VM.lua
@@ -1,7 +1,12 @@
 local RS = game:GetService("ReplicatedStorage")
 local DD = RS:WaitForChild("Remotes"):FindFirstChild("DD")
 
-
-DD.OnServerEvent:Connect(function(player, Hum, rayResult)
-    
+DD.OnServerEvent:Connect(function(player, Hum, rayResult, cur)
+    local Damage = cur:FindFirstChild("Damage")
+    local Amm = cur:FindFirstChild("Amm")
+        
+    if Amm.Value > 0 and Hum.Health > 0 then
+        Amm.Value -= 1
+        Hum.Health -= Damage
+    end
 end)
