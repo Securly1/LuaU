3585d16a51bfd1248defcb3fb579097d8a19a005
Securly <91905863+Securly1@users.noreply.github.com>
2021-10-19 13:02:33 -0400

    Update DD_VM.lua

--- DD_VM.lua
+++ DD_VM.lua
@@ -1,1 +1,7 @@
+local RS = game:GetService("ReplicatedStorage")
+local DD = RS:WaitForChild("Remotes"):FindFirstChild("DD")
 
+
+DD.OnServerEvent:Connect(function(player, Hum, rayResult)
+    
+end)
