d5c2bd8f827a002a750252236408f817fe0c0f3d
Securly <91905863+Securly1@users.noreply.github.com>
2021-10-19 16:25:38 -0400

    Update ViewmodelBasics.lua

--- ViewmodelBasics.lua
+++ ViewmodelBasics.lua
@@ -23,56 +23,59 @@ local CanRe = true
 local ShootingAnim = "AnimId"
 local ReAnim = "AnimId"
 
-Mouse.MouseButton1Down:Connect(function()
-    if CanFire and not IsReloading then
-        CanFire = false
-        CanRe = false
-        local NewAnimShoot = Instance.New("Animation")
-        NewAnimShoot.AnimationId = ShootingAnim
-        NewAnimShoot.Parent = Character:FindFirstChild("Animations")
-   
-        if Character:WaitForChild("Humanoid") then
-            local LoadAnimShoot = Character:WaitForChild("Humanoid"):LoadAnimation(NewAnimShoot)
-            local DD = game:GetService("ReplicatedStorage"):WaitForChild("DD")
-            
-            LoadAnimShoot:Play()
-            
-            local RayCast = workspace:RayCast(Camera.CFrame, Camera.CFrame.LookVector * Range, RaycastParameters.New())
-                
-            if RayCast and RayCast.Instance.Parent:FindFirstChildOfClass("Humanoid") then
-                local HitHum = RayCast.Instance.Parent:FindFirstChildOfClass("Humanoid")
-                    
-                DD:FireSever(HitHum)
-            end
-            LoadAnimShoot.Stopped:Wait()
-            CanFire = true
-            
-        end
-    end
+
+Mouse.Button1Down:Connect(function()
+	if CanFire and not IsReloading then
+		CanFire = false
+		CanRe = false
+		local NewAnimShoot = Instance.new("Animation")
+		NewAnimShoot.AnimationId = ShootingAnim
+		NewAnimShoot.Parent = Character:FindFirstChild("Animations")
+
+		if Character:WaitForChild("Humanoid") then
+			local LoadAnimShoot = Character:WaitForChild("Humanoid"):LoadAnimation(NewAnimShoot)
+			--local DD = game:GetService("ReplicatedStorage"):WaitForChild("DD")
+
+			LoadAnimShoot:Play()
+
+			local RayCast = workspace:RayCast(Camera.CFrame, Camera.CFrame.LookVector * Range, RaycastParams.new())
+
+			if RayCast and RayCast.Instance.Parent:FindFirstChildOfClass("Humanoid") then
+				local HitHum = RayCast.Instance.Parent:FindFirstChildOfClass("Humanoid")
+
+				--DD:FireSever(HitHum)
+			end
+			LoadAnimShoot.Stopped:Wait()
+			CanFire = true
+
+		end
+	end
 end)
 
-USerInputService.InputBegan:Connect(function(input)
-    if input.KeyCode == Enum.KeyCode.R then
-        if not IsReloading then
-            local NewR = Instance.New("Animation")
-            NewR.AnimationId = ReAnim
-            NewR.Parent = Character:FindFirstChild("Animations")
-                
-            CanFire = false
-            CanRe = false
-            LoadR = Character:WaitForChild("Humanoid"):LoadAnimation(NewR)
-             
-        end
-    end
+UserInputService.InputBegan:Connect(function(input)
+	if input.KeyCode == Enum.KeyCode.R then
+		if not IsReloading then
+			local NewR = Instance.new("Animation")
+			NewR.AnimationId = ReAnim
+			NewR.Parent = Character:FindFirstChild("Animations")
+
+			CanFire = false
+			CanRe = false
+			local LoadR = Character:WaitForChild("Humanoid"):LoadAnimation(NewR)
+			LoadR:Play()
+
+		end
+	end
 end)
 
 
 RunService.RenderStepped:Connect(function()
-    ViewModel:SetPrimaryPartCFrame(Camera.CFrame)
-        
-     for i, instances in pairs(Character:GetChildren())
-        if instances:IsA("BasePart") then
-           instances.LocalTransparency = 0 
-        end
-     end
+	local vm = Character:FindFirstChild("VM")
+	vm:SetPrimaryPartCFrame(Camera.CFrame)
+
+	for i, instances in pairs(vm:GetDescendants()) do
+		if instances:IsA("BasePart") or instances:IsA("UnionOperation") then
+			instances.LocalTransparencyModifier = 0
+		end
+	end
 end)
